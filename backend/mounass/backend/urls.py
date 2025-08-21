5d910817739ebf2c3736f99a46879df3c388e336
infos7mounass-ux <infos7mounass@gmail.com>
2025-08-21 15:57:20 +0200

    Update and rename backend_mounass_backend.urls.py to backend/mounass/backend/urls.py

--- backend/mounass/backend/urls.py
+++ backend/mounass/backend/urls.py
@@ -10,3 +10,12 @@ urlpatterns = [
     path('api/realestate/', include('realestate.urls')),
     path('api/payments/', include('payments.urls')),
 ]
+from django.urls import path, include
+
+urlpatterns = [
+    path('api/users/', include('users.urls')),
+    path('api/meet/', include('meet.urls')),
+    path('api/texts/', include('texts.urls')),
+    path('api/marketplace/', include('marketplace.urls')),
+    path('api/realestate/', include('realestate.urls')),
+]
